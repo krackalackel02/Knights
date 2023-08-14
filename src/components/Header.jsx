10a12a38725c42013f7bf9259c777ee0d9c7e5dd
Paul Kallarackel <108292404+krackalackel02@users.noreply.github.com>
2023-08-14 18:50:09 +0100

    added header logo and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Chess from "../assets/chess.svg";
 import "./Header.css";
 
 export default function Header() {
 	return (
 		<header className="header">
+			<svg>
+				<use xlinkHref={Chess + "#chess"}></use>
+			</svg>
 			<h1 className="title">KnightMover.io</h1>
 		</header>
 	);
